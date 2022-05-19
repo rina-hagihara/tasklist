f9c9955aa094fce73f4fe3e69e24d44d8713b863
rina_ <rina_@DESKTOP-LR1P7G1>
2022-05-19 12:16:43 +0900

    「getAlltasks]を追加

--- src/controllers/IndexSevlet.java
+++ src/controllers/IndexSevlet.java
@@ -1,19 +1,26 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Tasks;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class IndexSevlet
  */
 @WebServlet("/index")
 public class IndexSevlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +29,21 @@ public class IndexSevlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Tasks> tasks = em.createNamedQuery("getAllTaskes", Tasks.class).getResultList();
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
+
+
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+        em.close();
+    }
 
 }
--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class DBUtill {
+public class DBUtil {
     private static final String PERSISTENSE_UNIT_NAME = "tasklist";
     private static EntityManagerFactory emf;
 
